1c3224d56927eec00b8a0d8e55e6219cbc831caa
jamesmackay <cleancode.solutions@gmail.com>
2016-01-30 21:40:59 +0000

    set up angular material

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,2 +1,13 @@
 module.exports = function(grunt) {
+    grunt.loadNpmTasks('grunt-wiredep');
+
+    grunt.initConfig({
+        wiredep: {
+            all:{
+                directory: 'client/application/bower_components',
+                bowerJson: grunt.file.readJSON('client/application/bower.json'),
+                src: 'client/index.html'   
+            }
+        }
+    });
 };
