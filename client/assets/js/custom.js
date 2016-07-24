da3754a523237565e41655e1de1fa737f45aa86b
jamesmackay <cleancode.solutions@gmail.com>
2016-07-24 21:44:35 +0000

    change text based on slide

--- client/assets/js/custom.js
+++ client/assets/js/custom.js
@@ -33,4 +33,41 @@ window
             );
         }
     );
+    
+    $('#carousel')
+    .bind(
+        'slide.bs.carousel',
+        function( e ){
+            var index = (
+                $(e.relatedTarget)
+                .index()
+            ); 
+            
+            if( index == 0 ){
+                
+                $('#web-design-details')
+                .removeClass(
+                    'hidden'
+                );
+                
+                $('#hosting-details')
+                .addClass(
+                    'hidden'
+                );
+            }
+            
+            if( index == 1 ){
+                
+                $('#web-design-details')
+                .addClass(
+                    'hidden'
+                );
+                
+                $('#hosting-details')
+                .removeClass(
+                    'hidden'
+                );
+            }
+        }
+    );
 };
